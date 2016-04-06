b6fa4677ad22a03533f93a81de6298432172a4be
Roland Paltz <roland.paltz@kraemer-it.de>
2016-04-06 16:22:10 +0200

    settingsInput multiple values

--- public/js/utils/SettingsInputDirective.js
+++ public/js/utils/SettingsInputDirective.js
@@ -14,6 +14,42 @@
         };
         
         function link(scope, element, attrs){
+            scope.data = {
+                input: ""
+            };
+            if(!scope.scheme.multiple){
+                scope.data.input = scope.returnData;
+                scope.$watch('data.input', function(){
+                    scope.returnData = scope.data.input;
+                });
+            } else {
+                if(!angular.isArray(scope.returnData)){
+                    scope.returnData = [];
+                }
+            }
+            scope.add = function (){
+                    console.log(scope.data);
+                    if((scope.returnData.indexOf(scope.data.input) < 0) && (scope.data.input != "")){
+                        scope.returnData.push(scope.data.input);
+                        scope.data.input = "";
+                    }
+                };
+            
+            scope.remove = function (value) {
+                var idx = scope.returnData.indexOf(value);
+
+                if (idx >= 0) {
+                    scope.returnData.splice(idx, 1);
+                }
+            };
+            
+            scope.getInputClass = function(){
+                return scope.form.input.$valid ? "" : "has-error";
+            };
+            
+            scope.getClass = function(){
+                return scope.scheme.multiple ? "col-sm-9" : "col-sm-12";
+            };
         }
     }
 })();
