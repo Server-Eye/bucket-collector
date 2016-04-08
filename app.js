403ecfdcdb254cdc4c173e4a29f7240b1ae98401
Roland Paltz <roland.paltz@kraemer-it.de>
2016-04-08 10:09:44 +0200

    base for electron app

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var app = require('commander');
+'use strict';
 
-app.version(require('./package.json').version)
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var window = null;
+
+var options = require('commander');
+
+options.version(require('./package.json').version)
         .option('-d, --development', 'Starts the application in development mode, which enables additional logging')
         .option('-c, --clean', 'Removes all settings and saved bucket data')
-        .option('-P, --port <n>', 'Set the port of the webinterface to <n>. Default: 8080')
         .option('-R, --reactionDir [path]', 'Set the path from which all reactions are loaded')
         .option('-r, --reactionDataDir [path]', 'Set the path where the raction-data is saved')
         .option('-b, --bucketDataDir [path]', 'Set the path where the runtime-data is saved')
@@ -11,10 +18,28 @@ app.version(require('./package.json').version)
         .parse(process.argv);
 
 function start() {
-    require('./lib/config').setConfig(app);
+    require('./lib/config').setConfig(options);
     require('./lib/config').appLogger.info("Starting application");
     require('./lib/webinterface').start();
     require('./lib/bucketCollector').start();
+    
+    app.on('ready', function(){
+        window = new BrowserWindow({
+            width: 800,
+            height: 600
+        });
+        window.loadURL('http://127.0.0.1:8080/');
+        
+        window.on('closed', function(){
+            window = null;
+        });
+    });
 }
+
+app.on('window-all-closed', function(){
+    if(process.platform != 'darwin'){
+        app.quit();
+    }
+});
 
 start();
