fc03c4fe85dae0c540f7082ac7e0f3ec344b0b9b
Roland Paltz <roland.paltz@kraemer-it.de>
2016-04-12 10:31:08 +0200

    Added tooltips in UI

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,5 +8,9 @@
     ]).constant('API_URL', {
         v1: "https://api.server-eye.de/1",
         v2: "https://api.server-eye.de/2"
-    });
+    }).config(['$uibTooltipProvider', function($uibTooltipProvider){
+        $uibTooltipProvider.options({
+            placement: "right"
+        });
+    }]);
 })();
